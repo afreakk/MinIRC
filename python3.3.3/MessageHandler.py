9b74b479902c62836166e7773a3e6e3d92f09aa2
afreak <laderud@hotmail.com>
2014-04-03 01:31:40 +0200

    removed33supportcuzitsbad

--- python3.3.3/MessageHandler.py
+++
@@ -1,8 +0,0 @@
-
-class MessageHandler(object):
-    def makeChannelMessage(self, message):
-        self.makeMessage("PRIVMSG "+self.channel+" :"+message)
-
-    def makeMessage(self,message):
-        newMessage = Message(message+'\n\r')
-        self.replies.append(newMessage)
